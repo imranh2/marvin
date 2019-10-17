2b34407f4afa5059b9e9b49999697c1e68716dfb
Imran Hussain <imranh@sucs.org>
2019-10-17 14:46:18 +0100

    Print some simple stats when bot is ready

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,4 @@
 module.exports = (client) => {
   console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Ready to serve on ${client.guilds.size} servers, for ${client.users.size-1} users.`);
 };
